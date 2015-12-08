c20ea5ec5a53d8cf81e58c6df38fd4eaf06f6f3e
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-12-08 11:38:07 -0500

    Initial Commit

---
+++ tp6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tp6
+//
+//  Created by Hannah Stucken Douglas on 12/8/15.
+//  Copyright (c) 2015 67442. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
