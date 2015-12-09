4329507b7d1ac64220e2784369978c062302a0e0
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-12-09 11:40:57 -0500

    Webpage appearing but not adhering to the constraints of the page.

--- tp6/LearnMoreViewController.swift
+++ tp6/LearnMoreViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
-class LearnMoreViewController: UIViewController {
+class LearnMoreViewController: UIViewController, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView()
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = NSURL(string: "https://www.plannedparenthood.org/teens/my-body")
+        webView.loadRequest(NSURLRequest(URL: url!))
+
 
         // Do any additional setup after loading the view.
     }
@@ -20,16 +33,5 @@ class LearnMoreViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
